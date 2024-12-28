f44329f27237910bda526e9168b6bde6503130df
arlen <arlenismailov02@gmail.com>
2024-12-28 14:18:29 +0600

    #7 – Библиотека aiogram для разработки ботов

--- main.py
+++ main.py
@@ -1,61 +1,54 @@
-import telebot
-from currency_converter import CurrencyConverter
-from telebot import types
-# @ffafde_bot
-bot = telebot.TeleBot('7651063544:AAF2gCfraQfZi4n5ISc1hk-Aiwpe3428Pk0')
-currency = CurrencyConverter()
-amount = 0
+# Импортируем необходимые классы и функции из библиотеки aiogram
+from aiogram import Bot, Dispatcher, executor, types
+# @fafafafafaeweww_bot
+# Создаем экземпляр бота с токеном
+bot = Bot('7222878978:AAF9VNR2rrzV-u3W9XyEDIyP2xdKiACc2po')
+
+# Создаем диспетчер для обработки сообщений и других событий
+dp = Dispatcher(bot)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Привет, введите сумму')
-    bot.register_next_step_handler(message, summa)
+# Обработчик сообщений с содержимым типа "фото"
+@dp.message_handler(content_types=['photo'])  # Указываем, что обрабатываем только сообщения с фото
+async def start(message: types.Message):
+    # Отправляем ответное сообщение с текстом "Hello"
+    await message.reply('Hello')
+    # Пример кода для отправки фото (закомментирован):
+    # file = open('/some.png', 'rb')
+    # await message.answer_photo(file)
 
 
-def summa(message):
-    global amount
-    try:
-        amount = int(message.text.strip())
-    except ValueError:
-        bot.send_message(message.chat.id, 'Неверный формат. Впишите сумму')
-        bot.register_next_step_handler(message, summa)
-        return
-
-    if amount > 0:
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        btn1 = types.InlineKeyboardButton('USD/EUR', callback_data='usd/eur')
-        btn2 = types.InlineKeyboardButton('EUR/USD', callback_data='eur/usd')
-        btn3 = types.InlineKeyboardButton('USD/GBP', callback_data='usd/gbp')
-        btn4 = types.InlineKeyboardButton('Другое значение', callback_data='else')
-        markup.add(btn1, btn2, btn3, btn4)
-        bot.send_message(message.chat.id, 'Выберите пару валют', reply_markup=markup)
-    else:
-        bot.send_message(message.chat.id, 'Число должно быть больше 0, введите сумму')
-        bot.register_next_step_handler(message, summa)
+# Обработчик команды "/inline"
+@dp.message_handler(commands=['inline'])
+async def info(message: types.Message):
+    # Создаем разметку для встроенных кнопок
+    markup = types.InlineKeyboardMarkup()
+    # Добавляем кнопку, которая ведет на сайт
+    markup.add(types.InlineKeyboardButton('Site', url='https://itproger.com'))
+    # Добавляем кнопку с callback-данными
+    markup.add(types.InlineKeyboardButton('Hello', callback_data='hello'))
+    # Отправляем сообщение с кнопками
+    await message.reply('Hello', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    if call.data != 'else':
-        values = call.data.upper().split('/')
-        res = currency.convert(amount, values[0], values[1])
-        bot.send_message(call.message.chat.id, f'Получается: {round(res, 2)}. Можете заново вписать сумму')
-        bot.register_next_step_handler(call.message, summa)
-    else:
-        bot.send_message(call.message.chat.id, 'Введите пару значений через слеш')
-        bot.register_next_step_handler(call.message, my_currency)
+# Обработчик callback-запросов (нажатие на Inline-кнопки)
+@dp.callback_query_handler()
+async def callback(call):
+    # Отправляем сообщение с callback-данными, которые содержались в кнопке
+    await call.message.answer(call.data)
 
 
-def my_currency(message):
-    try:
-        values = message.text.upper().split('/')
-        res = currency.convert(amount, values[0], values[1])
-        bot.send_message(message.chat.id, f'Получается: {round(res, 2)}. Можете заново вписать сумму')
-        bot.register_next_step_handler(message, summa)
-    except Exception:
-        bot.send_message(message.chat.id, 'Что-то не так. Впишите значение заново')
-        bot.register_next_step_handler(message, my_currency)
+# Обработчик команды "/reply"
+@dp.message_handler(commands=['reply'])
+async def reply(message: types.Message):
+    # Создаем разметку для кнопок, отображающихся прямо в чате
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)  # Клавиатура исчезает после выбора
+    # Добавляем кнопки "Site" и "Website"
+    markup.add(types.KeyboardButton('Site'))
+    markup.add(types.KeyboardButton('Website'))
+    # Отправляем сообщение с кнопками
+    await message.answer('Hello', reply_markup=markup)
 
 
-bot.polling(none_stop=True)
+# Запуск бота с использованием метода start_polling
+executor.start_polling(dp)
